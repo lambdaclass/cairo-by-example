719df7aeb2fd4ba26b7755cad8a58f75a3d1e986
Klaus Lungwitz <18153834+klaus993@users.noreply.github.com>
2023-06-21 17:29:44 -0300

    Fix

--- homebrew/cairo.rb
+++ homebrew/cairo.rb
@@ -1,15 +1,17 @@
 class Cairo < Formula
   desc "Cairo language installation"
   homepage "https://cairo-by-example.com/"
-  head do
-    url "https://github.com/starkware-libs/cairo", branch: "main"
-  end
-  # version "0.0.1"
-  # sha256 ""
+  url "https://github.com/starkware-libs/cairo/archive/refs/tags/v1.1.0.tar.gz"
+  sha256 "e13ff5a2375e3520f4f1f5fa7d251f362ea42c428490ef15137460bdac637bb4"
+  version "v1.1.0"
   license "MIT"
 
   def install
-    system "echo", "Hello"
+    ENV.prepend_path "PATH", "/Users/#{ENV["USER"]}/.cargo/bin"
+    `rustup override set stable`
+    `rustup update`
+    `cargo build --all --release --manifest-path ./Cargo.toml`
+    bin.install "./target/release/cairo-run"
   end
 
   test do

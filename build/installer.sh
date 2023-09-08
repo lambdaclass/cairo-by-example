e39f1b498f669d7af9e439495fad3c5201d19c16
samoht9277 <totobighouse@gmail.com>
2023-09-08 12:33:20 -0300

    Typo in --noconfirm flag to yay.

--- build/installer.sh
+++ build/installer.sh
@@ -69,7 +69,7 @@ install() {
 		echo -e "${LOG}Installing Cairo ${CAIRO_VERSION} on Arch Linux ${RST}"
 		[[ ! -e /usr/bin/yay ]] && echo -e "${ERR}No yay installation found. If you use another AUR helper, please install the cairo-lang package.${RST}"
 		set -x
-		yay -S --no-confirm cairo-lang
+		yay -S --noconfirm cairo-lang
 		set +x
 		set_cairo_corelib "/usr/lib/corelib/"
 		;;
